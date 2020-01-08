14d7e7c6e7af5cd6a3f9cf174a549c2ca6f4dc73
Ymin-dgnn <57888020+Ymin-dgnn@users.noreply.github.com>
2020-01-08 17:53:28 +0900

    mainscreen first commit

---
+++ mainScreen_Lim/mainScreen_Lim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mainScreen_Lim
+//
+//  Created by 임연지 on 2020/01/08.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
