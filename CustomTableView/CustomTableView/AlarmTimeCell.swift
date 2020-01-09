13d6845f5913248b68e5f1f0bb6e9cfc6e4d8523
Ymin-dgnn <57888020+Ymin-dgnn@users.noreply.github.com>
2020-01-09 16:26:39 +0900

    alarmtimeset table view

---
+++ CustomTableView/CustomTableView/AlarmTimeCell.swift
@@ -0,0 +1,29 @@
+//
+//  AlarmTimeCell.swift
+//  CustomTableView
+//
+//  Created by dgulinc on 2020/01/09.
+//  Copyright © 2020 dgulinc. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTimeCell: UITableViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+
+/*    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }*/
+
+}
