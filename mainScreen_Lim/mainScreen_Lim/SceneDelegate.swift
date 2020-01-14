c91fb6edaec6add77ef7f82de1233e2886454703
sheepjin99 <choyj1222@gmail.com>
2020-01-14 11:47:21 +0900

    credit updates

--- mainScreen_Lim/mainScreen_Lim/SceneDelegate.swift
+++ mainScreen_Lim/mainScreen_Lim/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  mainScreen_Lim
 //
-//  Created by 임연지 on 2020/01/08.
-//  Copyright © 2020 임연지. All rights reserved.
+//  Created by 조양진 임연지 김완기 on 2020/01/08.
+//  Copyright © 2020 조양진 임연지 김완기. All rights reserved.
 //
 
 import UIKit
