7b512e90c0b1c7051077f31abb862f5f14b86d5a
Ymin-dgnn <57888020+Ymin-dgnn@users.noreply.github.com>
2020-01-21 14:35:15 +0900

    Title problem solved

--- SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/MainViewController.swift
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/MainViewController.swift
@@ -12,27 +12,6 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //카이틀 속성설정
-        let title = UILabel()
-        title.text = "Sleep With AI"
-        //title.textAlignment = .left
-        title.textColor = UIColor.white
-        //title.frame.origin.x = self.view.frame.width - title.frame.width
-
-        if let font = UIFont(name:"Chalkduster" , size: 35) {
-                
-            let fontMetrics = UIFontMetrics(forTextStyle: .body)
-            title.font = fontMetrics.scaledFont(for: font)
-        }
-        //만들어진 타이틀 레이블을 네비게이션바 안에 들어가게함
-        self.navigationItem.titleView = title
-        
-        //타이틀뷰의 위치를 왼쪽으로 조정하고 싶었지만 되지 않는 코드
-        if let titleView = self.navigationItem.titleView{
-            titleView.frame.origin.x = self.view.frame.width - titleView.frame.width
-        }
-
 
         // Do any additional setup after loading the view.
     }
