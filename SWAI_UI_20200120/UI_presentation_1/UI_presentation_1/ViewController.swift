060ad488b41438ac39e0dae3d3b8602d05ad9c98
KimWanki <57824307+KimWanki@users.noreply.github.com>
2020-01-23 19:20:26 +0900

    연지가 빌드가 안된데요.

---
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  UI_presentation_1
+//
+//  Created by 임연지 on 2020/01/15.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var index: Int?
+    var data =
+        [
+        ["⚽️ Soccer",       "⛳️ Golf",      "🏀 Basketball",    "🏈 American Football",
+         "⚾️ Baseball",     "🎾 Tennis",    "🏐 Valleyball",    "🏸 Badminton"],
+        ["🍎 Apple",        "🍐 Pear",      "🍓 Strawberry",    "🥑 Avocado",
+         "🍌 Banana",       "🍇 Grape",     "🍈 Melon",         "🍊 Orange",
+         "🍑 Peach",        "🥝 Kiwi"]
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nibName = UINib(nibName: "CustomCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "customCell")
+    }
+    @IBAction func switchSegmentedCtrlViewAction(_ sender: UISegmentedControl) {
+        self.index = sender.selectedSegmentIndex
+        tableView.reloadData()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[self.index ?? 0].count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomCell
+        let str = data[self.index ?? 0][indexPath.row].components(separatedBy: " ")
+        cell.custominit(text: str[1], accessoryText: str[0])
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+}
