1df5fb58bb55c3e9491bcea7c24169b9ea7344d8
KimWanki <57824307+KimWanki@users.noreply.github.com>
2020-01-24 16:38:18 +0900

    datatab segue to description

---
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/DataTabDescViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DataTabDescViewController.swift
+//  UI_presentation_1
+//
+//  Created by WANKI KIM on 2020/01/24.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+
+import UIKit
+
+class DataTabDescViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+        // Do any additional setup after loading the view.
+    }
+}
