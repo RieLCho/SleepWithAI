a7d3e20c32bd32e5ac370776e3ca404079fd0a92
RieL <choyj1222@gmail.com>
2020-01-28 00:54:56 +0900

    CustomViewControllers

---
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/AgeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AgeInputController.swift
+//  UI_presentation_1
+//
+//  Created by Yangjin Cho on 2020/01/28.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+import UIKit
+import Foundation
+
+
+class AgeViewController: UIViewController {
+    
+    @IBOutlet weak var AgeInputTextField: UITextField!
+    
+    
+}
---
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/AlarmTimeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AlarmTimeViewController.swift
+//  UI_presentation_1
+//
+//  Created by Yangjin Cho on 2020/01/28.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class AlarmTimeViewController: UIViewController {
+    
+    @IBOutlet weak var EveryMonday: UIDatePicker!
+    @IBOutlet weak var EveryTuesday: UIDatePicker!
+    @IBOutlet weak var EveryWednesday: UIDatePicker!
+    @IBOutlet weak var EveryThursday: UIDatePicker!
+    @IBOutlet weak var EveryFriday: UIDatePicker!
+    @IBOutlet weak var EverySaturday: UIDatePicker!
+    @IBOutlet weak var EverySunday: UIDatePicker!
+    
+    
+}
