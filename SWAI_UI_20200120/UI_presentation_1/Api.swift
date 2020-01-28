3f91613c7c527c18492f9e4fdf650bb1d5b64243
KimWanki <57824307+KimWanki@users.noreply.github.com>
2020-01-29 02:04:44 +0900

    input soundClassification into mainview modal
    사운드 분석에 관한 classification model을 뷰에 삽입.
    change app target to audio & air

--- SWAI_UI_20200120/UI_presentation_1/Api.swift
+++ SWAI_UI_20200120/UI_presentation_1/Api.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2020 임연지. All rights reserved.
 //
 
-import Foundation
-
-class MyApi: NSObject {
-    static let shared = MyApi()
-
-    //비동기 함수에 있는 결과가 완료가 되면 그 완료된 데이터를 다시 알려준다.
-    func allMenu(completion: @escaping ([DayData]) -> Void) { //}, onError: @escaping (Error) -> Void) {
-        let result = [
-            Daydata(day: "2020-01-25")
-        ]
-        //서버다녀오기나 coreML작업 등 시간이 많이 소요되는 작업을 하면 작업이 끝나고 나서 뷰컨트롤러에 알려줌 다 이런식으로 바꿔서 먼저 작업을 해두자
-        completion(result)
-    }
-}
+//import Foundation
+//
+//class MyApi: NSObject {
+//    static let shared = MyApi()
+//
+//    //비동기 함수에 있는 결과가 완료가 되면 그 완료된 데이터를 다시 알려준다.
+//    func allMenu(completion: @escaping ([DayData]) -> Void) { //}, onError: @escaping (Error) -> Void) {
+//        let result = [
+//            Daydata(day: "2020-01-25")
+//        ]
+//        //서버다녀오기나 coreML작업 등 시간이 많이 소요되는 작업을 하면 작업이 끝나고 나서 뷰컨트롤러에 알려줌 다 이런식으로 바꿔서 먼저 작업을 해두자
+//        completion(result)
+//    }
+//}
---
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/MainModalViewController.swift
@@ -0,0 +1,126 @@
+//
+//  MainModalViewController.swift
+//  UI_presentation_1
+//
+//  Created by WANKI KIM on 2020/01/29.
+//  Copyright © 2020 임연지. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import SoundAnalysis
+
+
+class MainModalViewController: UIViewController {
+    
+    private let audioEngine = AVAudioEngine()
+    private var soundClassifier = SleepSoundClassification()
+    
+    var inputFormat: AVAudioFormat!
+    var analyzer: SNAudioStreamAnalyzer!
+    var resultsObserver = ResultsObserver()
+    let analysisQueue = DispatchQueue(label: "com.custom.AnalysisQueue")
+    
+    let transcribedText:UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .center
+        view.textAlignment = .center
+        view.numberOfLines = 0
+        view.font = UIFont.systemFont(ofSize: 20)
+        return view
+    }()
+    
+    let placeholderText:UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .center
+        view.textAlignment = .center
+        view.numberOfLines = 0
+        view.font = UIFont.systemFont(ofSize: 25)
+        return view
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        resultsObserver.delegate = self
+        inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+        analyzer = SNAudioStreamAnalyzer(format: inputFormat)
+        
+        buildUI()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        startAudioEngine()
+    }
+    func buildUI()
+    {
+        self.view.addSubview(placeholderText)
+        self.view.addSubview(transcribedText)
+        
+        NSLayoutConstraint.activate(
+            [transcribedText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 350),
+             transcribedText.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+             transcribedText.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+             transcribedText.heightAnchor.constraint(equalToConstant: 100),
+             transcribedText.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [placeholderText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+             placeholderText.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+             placeholderText.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+             placeholderText.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ]
+        )
+    }
+    private func startAudioEngine() {
+        do {
+            let request = try SNClassifySoundRequest(mlModel: soundClassifier.model)
+            try analyzer.add(request, withObserver: resultsObserver)
+        } catch {
+            print("Unable to prepare request: \(error.localizedDescription)")
+            return
+        }
+        
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 8000, format: inputFormat) { buffer, time in
+            self.analysisQueue.async {
+                self.analyzer.analyze(buffer, atAudioFramePosition: time.sampleTime)
+            }
+        }
+        
+        do{
+            try audioEngine.start()
+        }catch( _){
+            print("error in starting the Audio Engin")
+        }
+    }
+    
+}
+protocol SoundClassifierDelegate {
+    func displayPredictionResult(identifier: String, confidence: Double)
+}
+
+extension MainModalViewController: SoundClassifierDelegate {
+    func displayPredictionResult(identifier: String, confidence: Double) {
+        DispatchQueue.main.async {
+            self.transcribedText.text = ("Recognition: \(identifier)\nConfidence \(confidence)")
+        }
+    }
+}
+
+
+class ResultsObserver: NSObject, SNResultsObserving {
+    var delegate: SoundClassifierDelegate?
+    func request(_ request: SNRequest, didProduce result: SNResult) {
+        guard let result = result as? SNClassificationResult,
+            let classification = result.classifications.first else { return }
+        
+        let confidence = classification.confidence * 100.0
+        
+        if confidence > 60 {
+            delegate?.displayPredictionResult(identifier: classification.identifier, confidence: confidence)
+        }
+    }
+}
