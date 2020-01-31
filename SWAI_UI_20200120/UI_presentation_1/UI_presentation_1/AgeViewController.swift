f97aafc9fdff593721d4dc2c7d3732f021f89f44
RieL <choyj1222@gmail.com>
2020-01-31 15:44:22 +0900

    setting tab alert

--- SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/AgeViewController.swift
+++ SWAI_UI_20200120/UI_presentation_1/UI_presentation_1/AgeViewController.swift
@@ -7,12 +7,26 @@
 //
 import UIKit
 import Foundation
-//
-//
 
 class AgeViewController: UIViewController {
-//    
-//    @IBOutlet weak var AgeInputTextField: UITextField!
-//    
-//    
-}
+    
+    @IBOutlet weak var ageInputTextField: UITextField!
+    
+    @IBOutlet weak var settedUserAgeLabel: UILabel!
+    
+    @IBOutlet weak var ageInputSaveButton: UIBarButtonItem!
+    
+    @IBAction func showAlert() {
+        // UIAlertController를 생성해야 한다. style은 .alert로 해준다.
+        let alertController = UIAlertController(title: "User Age Saved!", message: "Your age has been saved.", preferredStyle: .alert)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func ageInputSaveButtonClicked(_ sender: Any?) {
+        let userAge = ageInputTextField.text
+        print(userAge)
+        showAlert()
+        }
+    }
+    
+
